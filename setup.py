40d074b1509f6be747f34e3d07e9e719c0b08e8b
Stefan Wildschut <stefanwildschut4@gmail.com>
2019-03-19 17:40:01 +0200

    Second Commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='EDSA Hackathon python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
+    url='https://github.com/<StefanW777>/<stefanspackage>',
     author='Stefan Wildschut',
     author_email='stefanwildschut4@gmail.com'
 )
--- tests/recursivetests.py
+++ tests/recursivetests.py
@@ -2,6 +2,6 @@
 
 
 
-            sum_array(np.array([8,20,90,56,23]))
-            sum_array(np.array([-1,-2,-6,-4,-55]))
-            sum_array(np.array([4,500,-116,-4,-55]))
+            sum_array([8,20,90,56,23])
+            sum_array([-1,-2,-6,-4,-55])
+            sum_array([4,500,-116,-4,-55])
